4ad496deac1f0717ed82f108adb0f0312baad5e2
matt sinton-hewitt <jo@Jos-MacBook-Pro.local>
2018-03-23 22:33:36 +0000

    Flow init ready to test

--- src/main/scala/play/river/FlowInitialiser.scala
+++ src/main/scala/play/river/FlowInitialiser.scala
@@ -5,44 +5,47 @@ import play.hex.BordersHex
 import play.hex.vertex.Point
 import play.utils.Utils
 
+import scala.annotation.tailrec
 import scala.util.Random
 
 
+// TODO rename as RiverFlowCreator?
 class FlowInitialiser(random: Random) extends LazyLogging {
 
-  type RemainingEdgesIndexed = IndexedSeq[BordersHex]
   type RemainingEdges = Iterable[BordersHex]
   type RiverMap = Map[BordersHex, RiverSegment]
 
-  def setup(edges: IndexedSeq[BordersHex]): RiverMap = {
-    val (remainingEdges, rivers) = setupFlowsFromSources(edges)
-    rivers ++ setupFlowsFromRandom(remainingEdges.toIndexedSeq)
+  def setup(edges: RemainingEdges): RiverMap = {
+    val (remainingEdges, rivers) = createFlowsFromSources(edges)
+    rivers ++ createRiversFromRandom(remainingEdges)
   }
 
   // A source is a point where there are 3 rivers connected
-  def setupFlowsFromSources(edges: RemainingEdgesIndexed): (RemainingEdges, RiverMap) = {
+  def createFlowsFromSources(edges: RemainingEdges): (RemainingEdges, RiverMap) = {
     val rivers = findSources(edges)
-      .flatMap(point => point.edges.map(edge => (edge, RiverSegment(Flow(point, edge.otherPoint(point))))))
+      .flatMap(point => point.edges.map(edge => (edge, RiverSegment(point, edge))))
       .toMap
 
     logger.debug(s"Got sources: $rivers")
     continueRiverFlow((edges.toSet -- rivers.keySet).toIndexedSeq, rivers)
   }
 
-  private final def setupFlowsFromRandom(edges: RemainingEdgesIndexed, rivers: RiverMap = Map.empty): Map[BordersHex, RiverSegment] = {
-    Utils.sample(edges, random)
-      .map({edge =>
+  @tailrec
+  private final def createRiversFromRandom(edges: RemainingEdges, rivers: RiverMap = Map.empty): Map[BordersHex, RiverSegment] = {
+    Utils.sample(edges.toIndexedSeq, random) match {
+      case Some(edge) =>
         val river = setRandomFlow(edge)
         val pivotPoint = river.flow.from
-        // get neighbours of pivotPoint and for each set flow
-        val candidateEdges = (river.flow.from.edges - edge).intersect(edges.toSet)
-        val nextRivers = candidateEdges
-          .map(e => (e, RiverSegment(Flow(pivotPoint, e.otherPoint(pivotPoint)))))
+        // get edges of pivotPoint without rivers and for each set flow
+        val nextRivers = remainingEdgesOf(pivotPoint, edges.toSet - edge)
+          .map(e => (e, RiverSegment(flowFrom = pivotPoint, e)))
           .toMap + (edge -> river)
 
         val (newEdges, newRivers) = continueRiverFlow(edges.toSet -- nextRivers.keySet, rivers ++ nextRivers)
-        setupFlowsFromRandom(newEdges.toIndexedSeq, newRivers)
-    }).getOrElse(rivers)
+        createRiversFromRandom(newEdges, newRivers)
+
+      case _ => rivers
+    }
   }
 
   private def setRandomFlow(edge: BordersHex): RiverSegment = {
@@ -50,113 +53,26 @@ class FlowInitialiser(random: Random) extends LazyLogging {
     RiverSegment(Flow(from, to))
   }
 
-  private def setFlowFrom(riverSegment: RiverSegment, fromPoint: Point): RiverSegment = ???
+  private final def remainingEdgesOf(point: Point, edges: RemainingEdges): Set[BordersHex] =
+    point.edges.intersect(edges.toSet)
 
-  private def setFlowTowards(riverSegment: RiverSegment, fromPoint: Point): RiverSegment = ???
-
+  @tailrec
   private final def continueRiverFlow(edges: RemainingEdges, rivers: RiverMap): (RemainingEdges, RiverMap) = {
-    // TODO make more readable with convenience method to create River
     val nextRivers = rivers.flatMap({
       case (_, river) =>
-        lazy val fromPoint = river.flow.to
-        fromPoint.edges.intersect(edges.toSet)
-          .map(e => (e, RiverSegment(Flow(fromPoint, e.otherPoint(river.flow.to)))))
+        val flowFrom = river.flow.to
+        remainingEdgesOf(flowFrom, edges)
+          .map(e => (e, RiverSegment(flowFrom, e)))
     })
 
     if (nextRivers.isEmpty) (edges, rivers)
     else continueRiverFlow(edges.toSet -- nextRivers.keySet, rivers ++ nextRivers)
   }
 
-  def findSources(edges: Iterable[BordersHex]): Set[Point] = {
-      edges.flatMap(e => Seq(
-        Point(e.hexPosition, e.side.clockwiseVertex),
-        Point(e.hexPosition, e.side.antiClockwiseVertex)
-      )).groupBy(identity)
+  def findSources(edges: RemainingEdges): Set[Point] = {
+    edges.flatMap(_.points)
+      .groupBy(identity)
       .filter(_._2.size > 2)
       .keySet
   }
 }
-
-/*
-
-class FlowInitialiser(random: Random) extends LazyLogging {
-
-  def setup(rivers: Iterable[RiverSegment]): Unit = {
-    setupFlowsFromSources(rivers)
-    setupFlowsFromRandom(rivers)
-  }
-
-  // A source is a point where there are 3 rivers connected
-  def setupFlowsFromSources(rivers: Iterable[RiverSegment]): Unit = {
-
-    val riversWithFlow = findSources(rivers)
-      .flatMap(point => point.getRivers.map(setFlowFrom(_, point)))
-
-    logger.debug(s"Got sources: $riversWithFlow")
-    continueRiverFlow(riversWithFlow.toList)
-  }
-
-  @tailrec
-  private final def setupFlowsFromRandom(rivers: Iterable[RiverSegment]): Unit = {
-
-    rivers.filter(r => r.flow.isEmpty) match {
-      case Nil =>
-
-      case riversWithoutFlow =>
-
-        val randomRiver = Utils.getRandom(riversWithoutFlow, random)
-        logger.debug(s"Setting random flow $randomRiver")
-
-        val next =
-          setRandomFlow(randomRiver).getNeighbour(inflowDirection = true)
-            .map(x => setFlowFrom(x._1, randomRiver.flow.get.to)) ++
-            randomRiver.getNeighbour(inflowDirection = false)
-              .map(y => setFlowTowards(y._1, randomRiver.flow.get.from))
-
-        logger.debug(s"Random flow next: $next")
-        continueRiverFlow(next.toList)
-
-        setupFlowsFromRandom(riversWithoutFlow)
-    }
-  }
-
-  private def setRandomFlow(river: RiverSegment): RiverSegment = {
-    val vertex = if (random.nextBoolean()) river.sideA.clockwiseVertex else river.sideA.anticlockwiseVertex
-    river.setFlowUsingFrom(river.hexA.vertices(vertex))
-    river
-  }
-
-  private def setFlowFrom(riverSegment: RiverSegment, fromPoint: Point): RiverSegment = {
-    riverSegment.setFlowUsingFrom(fromPoint)
-    riverSegment
-  }
-
-  private def setFlowTowards(riverSegment: RiverSegment, fromPoint: Point): RiverSegment = {
-    riverSegment.setFlowUsingFrom(fromPoint)
-    riverSegment
-  }
-
-  @tailrec
-  private final def continueRiverFlow(riversWithFlow: List[RiverSegment]): Unit = {
-
-    riversWithFlow match {
-      case Nil => logger.debug("<--- continue river flow finished")
-      case _ => continueRiverFlow(riversWithFlow.flatMap(r => {
-        val point = r.flow.get.to
-        point.getRivers.filter(_.flow.isEmpty).map(setFlowFrom(_, point))
-      }))
-    }
-  }
-
-  def findSources(rivers: Iterable[RiverSegment]): Set[Point] = {
-    rivers
-      .flatMap(r => Seq(
-        r.hexA.vertices(r.sideA.anticlockwiseVertex()),
-        r.hexA.vertices(r.sideA.clockwiseVertex())))
-      .groupBy(p => p)
-      .filter(_._2.size == 3)
-      .keySet
-  }
-}
-
- */
--- src/main/scala/play/river/RiverSegment.scala
+++ src/main/scala/play/river/RiverSegment.scala
@@ -1,7 +1,15 @@
 package play.river
 
+import play.hex.BordersHex
 import play.hex.vertex.Point
 
 case class RiverSegment(flow: Flow)
+
+object RiverSegment {
+
+  def apply(flowFrom: Point, edge: BordersHex): RiverSegment = {
+    RiverSegment(Flow(flowFrom, edge.otherPoint(flowFrom)))
+  }
+}
 
 case class Flow(from: Point, to: Point)
